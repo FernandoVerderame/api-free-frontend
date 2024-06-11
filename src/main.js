b0445c5d43bbfa556e951d2f18b6d51cb03995a2
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-11 11:31:18 +0200

    added router

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faInstagram);
 
+import AppLoader from './components/AppLoader.vue'
+import { router } from './router'
+
 const app = createApp(App)
+app.component('AppLoader', AppLoader)
 app.component('FontAwesomeIcon', FontAwesomeIcon);
+app.use(router)
 app.mount('#app')
