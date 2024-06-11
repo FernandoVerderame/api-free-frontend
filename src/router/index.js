3071703af580287bc2fe91bc66e29dab338aece3
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-11 11:40:34 +0200

    added post detail route

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 // Import pages
 import HomePage from '../pages/HomePage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
+import PostDetailPage from '../pages/PostDetailPage.vue';
 
 // Define routes
 const router = createRouter({
@@ -13,6 +14,11 @@ const router = createRouter({
             path: '/',
             component: HomePage,
             name: 'home'
+        },
+        {
+            path: '/posts/:slug',
+            component: PostDetailPage,
+            name: 'post-detail'
         },
         {
             path: '/not-found',
