86eaf5ad49fd69fe49a38fa9a3aeaa338c81ab88
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-11 10:48:21 +0200

    added PostCard

---
+++ src/components/posts/PostCard.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'PostCard',
+
+    props: {
+        post: Object
+    }
+}
+</script>
+
+<template>
+    <div class="col">
+        <div class="card m-5 text-center">
+            <img v-if="post.image" :src="post.image" :alt="post.title" class="card-img-top" style="height: 100px;">
+            <div class="card-body">
+                <h5 class="card-title mb-0">{{ post.title }}</h5>
+                <div class="create-date">Created on {{ post.createdAt }}</div>
+                <div class="d-flex justify-content-center"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
