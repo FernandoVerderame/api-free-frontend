7eae58d10a6a897f8b08f6910ff21f53b15c343b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-11 10:48:14 +0200

    added PostsList

---
+++ src/components/posts/PostsList.vue
@@ -0,0 +1,24 @@
+<script>
+import PostCard from './PostCard.vue';
+
+export default {
+    name: 'PostsList',
+
+    components: { PostCard },
+
+    props: {
+        posts: Array
+    }
+};
+</script>
+
+<template>
+    <section id="posts-list">
+        <div v-if="posts.length" class="row row-cols-3">
+            <PostCard v-for="post in posts" :key="post.id" :post="post" />
+        </div>
+        <h4 v-else>There aren't any Posts.</h4>
+    </section>
+</template>
+
+<style></style>
